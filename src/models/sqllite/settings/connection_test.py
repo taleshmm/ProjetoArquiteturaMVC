6f9af58075757f80a3fb947e988937d8eb127c3e
taleshmm <taleshmm@hotmail.com>
2025-04-03 15:18:02 -0300

    config: implementing models dir with connection to sqlite

---
+++ src/models/sqllite/settings/connection_test.py
@@ -0,0 +1,16 @@
+import pytest
+from sqlalchemy import Engine
+
+from .connection import db_connection_handler
+
+
+@pytest.mark.skip(reason="intereção com o banco de dados")
+def test_connect_db():
+    assert db_connection_handler.get_engine() is None
+    
+    db_connection_handler.connect_to_db()
+    db_engine = db_connection_handler.get_engine()
+    
+    assert db_engine is not None
+    assert isinstance(db_engine, Engine)
+    
